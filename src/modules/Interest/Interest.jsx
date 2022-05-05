771bdf8697aaacacf53ffd7b463e94d6d7c3dddd
Martin-Luther Obahor <kingm4159@gmail.com>
2022-05-05 09:55:15 +0100

    fix: interest localstorage bug

--- src/modules/Interest/Interest.jsx
+++ src/modules/Interest/Interest.jsx
@@ -1,71 +1,17 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import {useRouter} from "next/router";
 import axios from "axios";
 import styles from "./Interest.module.scss";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import Button from "../../components/Button/Button";
 import InputInterest from "../../components/inputFields/InputInterest/InputInterest";
 
-// const interests = [
-//   {
-//     id: 1,
-//     name: "Weddings",
-//     image: "./interests/wedding.png",
-//   },
-//   {
-//     id: 2,
-//     name: "Fashion",
-//     image: "./interests/fashion.png",
-//   },
-//   {
-//     id: 3,
-//     name: "Travel",
-//     image: "./interests/travel.png",
-//   },
-//   {
-//     id: 4,
-//     name: "Nature",
-//     image: "./interests/nature.png",
-//   },
-//   {
-//     id: 5,
-//     name: "Home Decor",
-//     image: "./interests/home_decor.png",
-//   },
-//   {
-//     id: 6,
-//     name: "Food",
-//     image: "./interests/food.png",
-//   },
-//   {
-//     id: 7,
-//     name: "Lifestyle",
-//     image: "./interests/lifestyle.png",
-//   },
-//   {
-//     id: 8,
-//     name: "Comedy",
-//     image: "./interests/comedy.png",
-//   },
-//   {
-//     id: 9,
-//     name: "Music",
-//     image: "./interests/music.png",
-//   },
-//   {
-//     id: 10,
-//     name: "Dance",
-//     image: "./interests/dance.png",
-//   },
-// ];
-
 function Interest() {
   const navigate = useNavigate();
   const [interests, setInterests] = useState([]);
   const [checkedInterestsState, setCheckedInterestsState] = useState([]);
   // eslint-disable-next-line no-unused-vars
-  const [_, setInterestState] = useLocalStorage("interests", []);
+  const [interestState, setInterestState] = useLocalStorage("interests", []);
   useEffect(() => {
     axios.get("https://eked.herokuapp.com/v1/api/interests").then(async (response) => {
       setInterests(response.data.data);
@@ -84,7 +30,9 @@ function Interest() {
   const addInterests = () => {
     // console.log(checkedInterestsState);
     setInterestState(() => [...checkedInterestsState]);
-    navigate("/home", { replace: true });
+    setTimeout(() => {
+      navigate("/home");
+    }, 1000);
   };
 
   return (
